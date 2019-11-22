f0cf04eab54879a151965c734ea3359344eb9102
iqiziqi <iqiziqi@outlook.com>
2019-11-22 17:41:52 +0800

    [fix] there are disconnect button everywhere.

--- src/ConnectionTreeItem.ts
+++ src/ConnectionTreeItem.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 
 export default class ConnectionTreeItem extends vscode.TreeItem {
 
+    public contextValue = 'connection-tree-item';
     public iconPath = {
         dark: resolve(__dirname, '../resources/dark/database.svg'),
         light: resolve(__dirname, '../resources/light/database.svg'),
--- src/KeyTreeItem.ts
+++ src/KeyTreeItem.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 
 export default class KeyTreeItem extends vscode.TreeItem {
 
+    public contextValue = 'key-tree-item';
+
     constructor(key: string) {
         super(key);
     }
